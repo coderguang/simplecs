2c9614598490cb5bd0a37fc850f42ffa5fef3c3a
royalchen <royalchen@royalchen.com>
2015-04-09 00:09:40 +0800

    客户端log测试
    Signed-off-by: royalchen <royalchen@royalchen.com>

---
+++ Client/gameClient0/Assets/Script/Log/Logger.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+public class Logger {
+
+    private static string file = "log.txt";
+    private static FileStream myFs;
+    private static StreamWriter mySw;
+
+    private static Logger _instance=null;
+
+    private Logger() {
+        Init();
+    
+    }
+
+    private void Init() {
+
+      //  if (!File.Exists(file)){
+            myFs = new FileStream(file, FileMode.Create);
+        //}
+        mySw = new StreamWriter(file, true, System.Text.Encoding.GetEncoding("utf-8"));
+        mySw.WriteLine("hello,this is client log");
+        System.Console.WriteLine("write ok");
+    }
+
+    public static void Log(string msg) {
+        mySw.WriteLine(msg);
+    }
+
+    public void Destroy() {
+        mySw.Close();
+        myFs.Close();
+    }
+
+    public static Logger GetInstance() { 
+        if(null==_instance)
+              _instance=new Logger();
+        return _instance;
+    
+    }
+
+}
---
+++ Client/gameClient0/Assets/Script/Log/testlog.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testlog : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Logger.GetInstance();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Logger.Log("hello");
+        Logger.Log("ok");
+	    
+	}
+}
