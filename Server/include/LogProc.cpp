c6038e5b3d460fe6a6bc16cbb7ac650b251e8e05
coderguang <royalchen@royalchen.com>
2015-04-08 22:11:29 +0800

    modified:   ../LogProc 	modified:   ../LogProc.cpp 	modified:   ../gt 	modified:   ../logg 	modified:   ../testlog.cpp

--- Server/include/LogProc.cpp
+++ Server/include/LogProc.cpp
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <queue>
 #include "Logger.h"
+#include "struct/shmLog.h"
 /**
  *this is the log process,the server only this one log process
  *user Posix shm and the sem
@@ -16,27 +17,12 @@
  */
 using namespace std;
 
-//the queue elements
-struct mLogMsg{
-	LogType type;
-	Level level;
-	//string msg;
-	char msg[256];
-};
-//const int MAXLOGQUEUE=100;
-//queue<mLogMsg> logQue;//the log queue in share memory
-
-struct shared{
-	sem_t *mmutex;//producer's semaphore when it push msg to queue
-	sem_t *producer;//it will post when producer works
-	sem_t *comsumer;//it will post when comsumer works
-
-};
 
 int main(int argc,char **argv){
 	int fd;
-	//queue<mLogMsg> *ptr;
-	mLogMsg *ptr;
+	int index;//the last msg index in msgdata
+	
+	struct shmLogMsg *ptr;
 
 	//delete the share memory if it exist!
 	shm_unlink("mlogShm");
@@ -47,37 +33,52 @@ int main(int argc,char **argv){
 		cout<<"create log share memory faild!"<<endl;
 	}
 	
-	cout<<"the struct length="<<sizeof(mLogMsg)<<endl;
 	
-	ftruncate(fd,sizeof(mLogMsg));
 	
 	//ptr=(queue<mLogMsg>*)mmap(NULL,sizeof(mLogMsg)*MAXLOGQUEUE,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-	ptr=(mLogMsg*)mmap(NULL,sizeof(mLogMsg),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+	ptr=(shmLogMsg*)mmap(NULL,sizeof(shmLogMsg),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+
+	ftruncate(fd,sizeof(struct shmLogMsg));
 
 	close(fd);
 
-	
-	sem_unlink("mlogMutex");
-	sem_unlink("mlogProducer");
-	sem_unlink("mlogComsumer");
-	
-	shared sharedSem;
-	sharedSem.mmutex=sem_open("mlogMutex",O_CREAT|O_EXCL|O_RDWR,0644,1);
-	sharedSem.producer=sem_open("mlogProducer",O_CREAT|O_EXCL|O_RDWR,0644,0);
-	//sharedSem.comsumer=sem_open("mlogComsumer",O_CREAT|O_EXCL|O_RDWR,0644,MAXLOGQUEUE);
-	sharedSem.comsumer=sem_open("mlogComsumer",O_CREAT|O_EXCL|O_RDWR,0644,1);
-
-	//comsumer works
-	while(true){
-		sem_wait(sharedSem.producer);//wait the producer's push msg to the queue
-		//mLogMsg temp=ptr->front();//get the front 
-		//Logger::GetInstance()->Log(temp.type,temp.level,temp.msg);//push it to the background thread's queue
-		//string m="helo";
-		string m=ptr->msg;
-		Logger::GetInstance()->Log(ptr->type,ptr->level,m);//push it to the background thread's queue
-		//ptr->pop();
-		sem_post(sharedSem.comsumer);
+	/*init the array of offset*/
+	for(index=0;index<MAXMSG;index++){
+		ptr->msgoff[index]=index*MSGSIZE;
 	}
+
+ /*init the semaphore in share memory*/
+	
+	sem_init(&ptr->mutex,1,1);
+	sem_init(&ptr->nempty,1,MAXMSG);
+	sem_init(&ptr->nstored,1,0);
+
+	/*this is the consumer*/
+
+	index=0;
+	while(true){
+		sem_wait(&ptr->nstored);
+		sem_wait(&ptr->mutex);
+		long offset=ptr->msgoff[index];
+		
+		printf("index=%d: %s\n",index,&ptr->msgdata[offset]);
+		string ms=(string)&ptr->msgdata[offset];
+		Logger::GetInstance()->Log(ptr->type[offset],ptr->level[offset],ms);
+		
+		if(++index>=MAXMSG)
+				index=0;
+		
+		sem_post(&ptr->mutex);
+		sem_post(&ptr->nempty);
+
+
+	}
+	exit(0);
 
 
 }
+/**
+ 				sem_unlink("mlogMutex");
+        sem_unlink("mlogProducer");
+        sem_unlink("mlogComsumer");
+*/
--- Server/include/testlog.cpp
+++ Server/include/testlog.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+#include <string.h>
 #include "LogInterface.h"
 #include <semaphore.h>
 #include <sys/types.h>
@@ -7,66 +8,55 @@
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include "Func.h"
-//#include "Logger.h"
+#include "struct/shmLog.h"
 using namespace std;
-
-
-struct mLogMsg{
-	LogType type;
-	Level level;
-	//string msg;
-	char msg[256];
-};
-//const int MAXLOGQUEUE=100;
-//queue<mLogMsg> logQue;
-
-struct shared{
-	sem_t *mmutex;
-	sem_t *producer;
-	sem_t *comsumer;
-};
 
 int main(){
 	int fd;
-	//queue<mLogMsg> *ptr;
-	mLogMsg *ptr;
 	
+	pid_t pid;	
+	struct shmLogMsg *ptr;
+	
+
 	fd=shm_open("mlogShm",O_RDWR,0644);
 	if(fd<0)
 		cout<<"open share memory faild"<<endl;
 	
-	//ptr=(queue<mLogMsg>*)mmap(NULL,sizeof(mLogMsg)*MAXLOGQUEUE,PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
-	ptr=(mLogMsg*)mmap(NULL,sizeof(mLogMsg),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
+	ptr=(shmLogMsg*)mmap(NULL,sizeof(shmLogMsg),PROT_READ|PROT_WRITE,MAP_SHARED,fd,0);
 	
 	close(fd);
 	
-	shared sharedSem;
-	sharedSem.mmutex=sem_open("mlogMutex",O_RDWR);
-	sharedSem.producer=sem_open("mlogProducer",O_RDWR);
-	sharedSem.comsumer=sem_open("mlogComsumer",O_RDWR);
-
-	pid_t pid;
 
 	for(int i=0;i<4;i++){
 		//if(fork()==0){
 			pid=getpid();
-			cout<<"come to fork "<<pid<<endl;
-			sem_wait(sharedSem.comsumer);
-			sem_wait(sharedSem.mmutex);
-			//string ms="i";
-			//ms+=IntToStr((int)pid);
-			//mLogMsg temp={DBLog,ERROR,ms};
-			//ptr=new mLogMsg{DBLog,ERROR};
-			ptr->type=DBLog;
-			ptr->level=ERROR;	
-			string ms="helo";
-			ptr->msg=ms.c_str();
-			//ptr->push(temp);
-			cout<<"push success"<<endl;
-			sem_post(sharedSem.mmutex);
-			sem_post(sharedSem.producer);
+			char mesg[MSGSIZE];
+			//snprintf(mesg,MSGSIZE,"",(long)pid,i,DBLog,ERROR);
+			
+			sem_wait(&ptr->nempty);
+			sem_wait(&ptr->mutex);
+			
+			long offset=ptr->msgoff[ptr->nput];
+
+			if(++(ptr->nput)>=MAXMSG)	
+				ptr->nput=0;
+			
+			sem_post(&ptr->mutex);
+			ptr->type[offset]=DBLog;
+			ptr->level[offset]=FATAL;
+			string s="hello";
+			s+=IntToStr((int)pid);
+			strcpy(&ptr->msgdata[offset],s.c_str());
+			sem_post(&ptr->nstored);
+
+		//	exit(0);
+
 		//}
+		
 	}
+	//exit(0);
+	while(true)
+		;
 		
 
 }
