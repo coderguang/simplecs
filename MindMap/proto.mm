5d49cebffc3495601f48a976912e7a94a783c81d
royalchen <royalchen@royalchen.com>
2015-04-09 10:43:02 +0800

    协议解析简单思维导图
    Signed-off-by: royalchen <royalchen@royalchen.com>

---
+++ MindMap/proto.mm
@@ -0,0 +1,17 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1428546600862" ID="ID_511549645" MODIFIED="1428547341407" TEXT="&#x534f;&#x8bae;&#x89e3;&#x6790;">
+<node CREATED="1428546701770" ID="ID_25450751" MODIFIED="1428546747760" POSITION="right" TEXT="&#x4e3a;&#x6bcf;&#x4e00;&#x4e2a;&#x5ba2;&#x6237;&#x65b0;&#x5efa;&#x4e00;&#x4e2a;&#x7528;&#x6237;&#x8fdb;&#x7a0b;&#xff0c;&#x5e76;&#x8fdb;&#x5165;while(true)&#x7b49;&#x5f85;&#x534f;&#x8bae;&#x9636;&#x6bb5;">
+<node CREATED="1428546620557" HGAP="112" ID="ID_644776901" MODIFIED="1428546769311" TEXT="&#x6536;&#x5230;&#x5ba2;&#x6237;&#x7aef;&#x7684;&#x534f;&#x8bae;" VSHIFT="-9">
+<node CREATED="1428546663753" ID="ID_1058802518" MODIFIED="1428546678433" TEXT="&#x8fdb;&#x5165;&#x534f;&#x8bae;&#x89e3;&#x6790;switch">
+<node CREATED="1428546681170" ID="ID_1015254455" MODIFIED="1428546695851" TEXT="&#x6839;&#x636e;&#x89e3;&#x6790;&#x5230;&#x7684;&#x534f;&#x8bae;&#x5185;&#x5bb9;&#x8fdb;&#x884c;&#x5bf9;&#x5e94;&#x7684;&#x64cd;&#x4f5c;"/>
+</node>
+</node>
+<node CREATED="1428546798545" HGAP="124" ID="ID_1663037990" MODIFIED="1428546811759" TEXT="&#x53d1;&#x534f;&#x8bae;&#x7ed9;&#x5ba2;&#x6237;&#x7aef;" VSHIFT="12">
+<node CREATED="1428546815649" ID="ID_853037125" MODIFIED="1428546830003" TEXT="&#x586b;&#x5145;&#x534f;&#x8bae;&#x5185;&#x5bb9;">
+<node CREATED="1428546831071" ID="ID_1603174416" MODIFIED="1428546837343" TEXT="&#x53d1;&#x9001;&#x5230;&#x5ba2;&#x6237;&#x7aef;"/>
+</node>
+</node>
+</node>
+</node>
+</map>
